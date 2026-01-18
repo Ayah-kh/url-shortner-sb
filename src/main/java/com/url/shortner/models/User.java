2dcbd0d807cb0bac58daf08a151641a6dca1590e
ayah <ayah.kh204@gmail.com>
2026-01-18 21:12:06 +0300

    add spring security and  UserDetailsImpl

--- src/main/java/com/url/shortner/models/User.java
+++ src/main/java/com/url/shortner/models/User.java
@@ -9,9 +9,10 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
 
-    @Id
+        @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String userName;
     private String email;
     private String password;
     private String role = "ROLE_USER";
